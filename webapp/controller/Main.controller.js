6e7717a32e78a31fc3f9c946cde8b58820c8d42a
P2000937359 <ferdinand.peeters@student.ap.be>
2018-11-30 10:51:58 +0000

    Final commit

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -8,9 +8,9 @@ sap.ui.define([
 	'sap/m/StandardListItem'
 ], function (Controller, MessageBox, JSONModel, Button, Dialog, List, StandardListItem) {
 	"use strict";
-	
+
 	return Controller.extend("com.flexso.HackTheFuture.controller.Main", {
-		
+
 		onInit: function () {
 			this.getIotData();
 		},
@@ -20,7 +20,7 @@ sap.ui.define([
 			// '/devices/XX/measures'  -> XX = your device id
 			var me = this;
 			var url = "/devices/98/measures?skip=0&top=100";
-        	var promise = new Promise(function (resolve, reject) {
+			var promise = new Promise(function (resolve, reject) {
 				$.ajax({
 					type: "GET",
 					url: url,
@@ -47,7 +47,7 @@ sap.ui.define([
 
 		groupData: function (data) {
 			var obj = new Object();
-			$.each(data, function(value, index, array){
+			$.each(data, function (value, index, array) {
 				var timestamp = data[value].timestamp.toString();
 				if (Object.keys(obj).indexOf(timestamp) == -1) {
 					obj[timestamp] = new Object();
@@ -56,22 +56,83 @@ sap.ui.define([
 				var measure = Object.keys(data[value].measure)[0];
 				obj[timestamp][measure] = data[value]["measure"][measure];
 			});
+			var me = this;
 			var json = JSON.stringify(obj);
 			var array = [];
-			Object.keys(obj).forEach(function(key) {
-    			array.push(obj[key]);
+			Object.keys(obj).forEach(function (key) {
+				var blob = me.base64toBlob(obj[key].artifact_signal);
+				obj[key].preview = URL.createObjectURL(blob);
+				array.push(obj[key]);
 			});
 			console.log(array);
 			var oModel = new sap.ui.model.json.JSONModel();
-			oModel.setData({modelData : array}); 
+			oModel.setData({
+				modelData: array
+			});
 			this.getView().setModel(oModel, "artifacts");
 		},
 
 		triggerML: function (oEvent) {
-			
+			var base64 = oEvent.getSource().getCustomData()[0].getProperty('value');
+			var blobUrl = URL.createObjectURL(this.base64toBlob(base64));
+			var xml = '' +
+				'<Dialog xmlns:mvc="sap.ui.core.mvc" xmlns="sap.ui.commons" xmlns:unified="sap.ui.unified" xmlns:table="sap.ui.table" title="Machine Learning">   ' +
+				'	<Image src="{/image}"></Image>                        ' +
+				'<Table noDataText="Drop column list items here and columns in the area above" id="table1" items="{/results}"><columns><Column id="column5"><header><Label text="Label" id="label10"/></header></Column><Column id="column6"><header><Label text="Score" id="label11"/></header></Column></columns><items><ColumnListItem><cells><Text text="{label}"/><Text text="{score}"/></cells></ColumnListItem></items></Table>'+
+				'	<buttons>                                                  ' +
+				'		<Button text="{/actionText}" press="closeDialog"/>     ' +
+				'	</buttons>                                                 ' +
+				'</Dialog>                                                     ';
+
+			/*** THIS IS THE "APPLICATION" CODE ***/
+
+			// create some dummy JSON data and create a Model from it
+			var data = {
+				imageurl: blobUrl,
+				actionText: "Close Dialog"
+			};
+			var oModel = new sap.ui.model.json.JSONModel();
+			oModel.setData(data);
+
+			// put a Button for opening the Dialog Fragment onto the screen
+			var oFragmentDialog;
+
+			var oDummyController = {
+				onInit: function () {
+					oView.byId("img_db").setSrc(reader.readAsDataURL(oModel.image))
+				},
+				closeDialog: function () {
+					oFragmentDialog.close();
+				}
+			};
+
+			var me = this;
+			var promise;
+			this.getMlAuthToken().then(function (token) {
+				promise = me.sendToMl(token, base64);
+				promise.then(function (result) {
+					var obj = {
+						result: result.predictions,
+						actionText: "Close Dialog",
+						image: blobUrl
+					};
+					alert(JSON.stringify(obj));
+					var oModel = new sap.ui.model.json.JSONModel();
+					oModel.setData(obj);
+					if (!oFragmentDialog) {
+						oFragmentDialog = sap.ui.xmlfragment({
+							fragmentContent: xml
+						}, oDummyController);
+						oFragmentDialog.setModel(oModel);
+					}
+					oFragmentDialog.open();
+				});
+			});
+
 		},
 
-		getMlAuthToken: function () {
+		getMlAuthToken: function (base64) {
+			var me = this;
 			var promise = new Promise(function (resolve, reject) {
 				$.ajax({
 					type: "GET",
@@ -96,16 +157,35 @@ sap.ui.define([
 			});
 		},
 
-		sendToMl: function () {
-		
-			//Use the following format to send to ML (image name can always be 'ArtifactSignal.jpg')
-			//image is a variable
-			//var formData = new FormData();
-			//formData.append("files", image, "ArtifactSignal.jpg");
-			
-			//url to post on : '/ml-dest/api/v2/image/classification/models/HTF/versions/2'
-			
-
+		sendToMl: function (token, base64) {
+			var contentType = 'image/jpg';
+			var image = this.base64toBlob(base64, contentType);
+			var blobUrl = URL.createObjectURL(image);
+			var formData = new FormData();
+			formData.append("files", image, "ArtifactSignal.jpg");
+			var promise = new Promise(function (resolve, reject) {
+				$.ajax({
+					type: "POST",
+					url: "/ml-dest/api/v2/image/classification/models/HTF/versions/2",
+					headers: {
+						"Accept": "application/json",
+						"APIKey": token,
+						"Authorization": token
+					},
+					success: function (data) {
+						resolve(data);
+					},
+					error: function (Error) {
+						reject((Error));
+					},
+					contentType: false,
+					async: false,
+					data: formData,
+					cache: false,
+					processData: false
+				});
+			});
+			return promise;
 		},
 
 		base64toBlob: function (b64Data, contentType, sliceSize) {
@@ -136,4 +216,3 @@ sap.ui.define([
 
 	});
 });
-
